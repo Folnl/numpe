4a9f0aecd24bc73d8c74076e482d455c2c7def04
Natan Boloni Brasileiro <natanbolonib@gmail.com>
2023-11-03 13:38:11 -0300

    FuelingOrderRepository

---
+++ api/src/main/java/br/com/numpetest/api/repositories/FuelingOrderRepository.java
@@ -0,0 +1,10 @@
+package br.com.numpetest.api.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.numpetest.api.domain.FuelingOrder;
+
+@Repository
+public interface FuelingOrderRepository extends JpaRepository<FuelingOrder, Integer> {
+}
